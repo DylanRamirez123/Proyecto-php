b2efcd1c968c741f811fc20e6d437759f1b5020c
DylanRamirez123 <dylanjoya101@gmail.com>
2023-04-30 17:50:50 -0500

    se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Alis Multi-styles</title>
+</head>
+<body>
+    <header id="header">
+		<div class="contenedor">
+			<div class="logo">
+				<img src="resource/logo/Alisonlf.png"alt="logo">
+			</div>
+			<nav class="menu">
+				<ul>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Estilos</a></li>
+					<li><a href="#">Trabajos</a></li>
+					<li><a href="#">Sobre nosotros</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</body>
+</html>
